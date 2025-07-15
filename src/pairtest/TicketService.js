cc08c1a6695138dae81975664aa21489d5409dcd
pbeardey <pbeardey@gmail.com>
2025-07-15 08:39:02 +0100

    refactor: logging

--- src/pairtest/TicketService.js
+++ src/pairtest/TicketService.js
@@ -14,34 +14,38 @@ export default class TicketService {
     this.#ticketTypeRequests = ticketTypeRequests;
 
     if (!Number.isInteger(this.#accountId) || this.#accountId < 1) {
-      logger.log({
-        level: 'error',
-        message: 'Account Id provided was not an integer greater than 0.',
-        request_id: this.#requestId,
-      });
+      this.#log().error(
+        'Account Id provided was not an integer greater than 0.',
+      );
       throw new InvalidPurchaseException(
         'Account Id must be a positive integer.',
       );
     }
 
     if (this.#ticketTypeRequests.length === 0) {
-      logger.log({
-        level: 'error',
-        message: 'Ticket type request is missing.',
-        request_id: this.#requestId,
-      });
+      this.#log().error('Ticket type request is missing.');
       throw new InvalidPurchaseException(
         'A least one ticket type must be requested.',
       );
     }
 
-    logger.log({
-      level: 'error',
-      message: 'Ticket type request is not of type ticketTypeRequest.',
-      request_id: this.#requestId,
-    });
+    this.#log().error('Ticket type request is not of type ticketTypeRequest.');
     throw new InvalidPurchaseException(
       'Ticket type request is not recognised.',
     );
   }
+
+  #log = () => {
+    const _logger = (level, message) => {
+      logger.log({
+        level,
+        message,
+        request_id: this.#requestId,
+      });
+    };
+
+    return {
+      error: (message) => _logger('error', message),
+    };
+  };
 }
