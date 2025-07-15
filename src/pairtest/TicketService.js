b847e8760b6b110dce2de889250b8f8356e14e34
pbeardey <pbeardey@gmail.com>
2025-07-15 07:58:52 +0100

    feat: throws error when no ticket type request is made

--- src/pairtest/TicketService.js
+++ src/pairtest/TicketService.js
@@ -5,17 +5,30 @@ import { randomUUID } from 'crypto';
 
 export default class TicketService {
   #requestId;
+  #accountId;
 
   purchaseTickets(accountId, ...ticketTypeRequests) {
     this.#requestId = randomUUID();
+    this.#accountId = accountId;
+
+    if (!Number.isInteger(this.#accountId) || this.#accountId < 1) {
+      logger.log({
+        level: 'error',
+        message: 'Account Id provided was not an integer greater than 0.',
+        request_id: this.#requestId,
+      });
+      throw new InvalidPurchaseException(
+        'Account Id must be a positive integer.',
+      );
+    }
 
     logger.log({
       level: 'error',
-      message: 'Account Id provided was not an integer greater than 0.',
+      message: 'Ticket type request is missing.',
       request_id: this.#requestId,
     });
     throw new InvalidPurchaseException(
-      'Account Id must be a positive integer.',
+      'A least one ticket type must be requested.',
     );
   }
 }
